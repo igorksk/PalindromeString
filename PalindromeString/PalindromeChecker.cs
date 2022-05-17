fff6e363c0c2df615f4e0df092eb5b8fc3a5d8e7
Igor <ihor.kaseko@aploris.com>
2022-05-17 13:23:16 +0300

    Add project files.

---
+++ PalindromeString/PalindromeChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeString
+{
+    internal class PalindromeChecker
+    {
+        public static bool CheckIfPalindrome(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            int[] letters = new int[128];
+
+            foreach(char c in s1)
+            {
+                letters[c]++;
+            }
+
+            foreach (char c in s2)
+            {
+                letters[c]--;
+
+                if (letters[c] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PalindromeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using PalindromeString;
+
+Console.WriteLine("Hello, World!");
+
+string test1 = "qwerty";
+string test2 = "qweryt";
+string test3 = "qwerab";
+
+Action<string, string> testFunc = (string s1, string s2) =>
+{
+    bool testResult = PalindromeChecker.CheckIfPalindrome(s1, s2);
+
+    Console.WriteLine("Strings " + s1 + " and " + s2
+        + (testResult ? " are palindrome" : " aren't palindrome"));
+};
+
+testFunc(test1, test2);
+testFunc(test1, test3);
